685ac105793b36d30bfbec344473b81a445bda32
Rachel Pinsker <rpinsker@fb.com>
2014-07-07 18:01:31 -0700

    made project

---
+++ RestaurantOrganizer/RestaurantOrganizerTests/RestaurantOrganizerTests.m
@@ -0,0 +1,34 @@
+//
+//  RestaurantOrganizerTests.m
+//  RestaurantOrganizerTests
+//
+//  Created by Rachel Pinsker on 7/7/14.
+//  Copyright (c) 2014 ___rpinsker___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RestaurantOrganizerTests : XCTestCase
+
+@end
+
+@implementation RestaurantOrganizerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
